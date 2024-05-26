b4f123787ff2be615947d3da1595af368722a7d7
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-05-26 16:37:54 +0530

    Time: 24 ms (51.35%), Space: 32.1 MB (77.84%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -18,11 +18,10 @@ public:
     }
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root==NULL) return NULL;
-        if(key>root->val) root->right= deleteNode(root->right,key);
-        else if(key<root->val) root->left= deleteNode(root->left,key);
-        
-        else
+        if(root->val==key)
         {
+            // if both child are present//
+            // Find the min of the right subtree and make the node val = root->right(min val) and delete the min value node from the right subtree//
             if(root->left!=NULL && root->right!=NULL)
             {
                 int minn= findmin(root->right);
@@ -31,15 +30,15 @@ public:
                 return root;
             
             }
-            if(root->left!=NULL) return root->left; // only right child of node to be deleted is present//
-            if(root->right!=NULL) return root->right;// only left child of node to be deleted is present//
+            if(root->left!=NULL) return root->left; // only left child is present//
+            if(root->right!=NULL) return root->right;// only right child is present//
             
-            // if both child are present
-            // Find the min of the right subtree and make the node val = root->right(min val) and delete the min value node from the right subtree
+           
             else return NULL;
-            
-            
         }
+        if(key>root->val) root->right= deleteNode(root->right,key);
+        else if(key<root->val) root->left= deleteNode(root->left,key);
+       
         
         return root;
             
