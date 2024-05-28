77af0d0944566c77eed798deb6e664067d16756f
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-05-29 00:11:08 +0530

    Time: 4 ms (32.92%), Space: 9.3 MB (66.26%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,37 +2,33 @@
 // SC=O(n)
 class Solution {
 public:
-        // Space Optimization code of House Robber II
-      int rob1(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
+     int solve(int st, int end , vector<int>&nums , vector<int>&dp)
+    
+    {
+      
 
-        int prev2=nums[0];
-        if(n==1) return prev2; // ⭐⭐This is the edge case in case of space optimization only
-        int prev1=0;
-        if(n>=2) prev1=max(nums[0],nums[1]); // ⭐⭐Edge case- dp[1] can only be intialize if the size of the n is greater than or equal  to 2 and intitlize the dp[1] = max(nums[1],nums[0])
-        for(int i=2;i<n;i++)
+        dp[st]=nums[st];
+        if(st+1<=end)dp[st+1]=max(nums[st],nums[st+1]); // ⭐⭐Edge case- dp[1] can only be intialize if the size of the n is greater than or equal  to 2 and intitlize the dp[1] = max(nums[1],nums[0])
+        for(int i=st+2;i<=end;i++)
         {
             // Take
-            int opt1=nums[i]+prev2;
+            int opt1=nums[i]+dp[i-2];
             // not take 
-            int opt2=prev1;
+            int opt2=dp[i-1];
 
-            int curr=max(opt1,opt2);
-            prev2=prev1;
-            prev1=curr;
+            dp[i]=max(opt1,opt2);
         }
-        return prev1;
+        return dp[end];
     }
     int rob(vector<int>& nums) {
-        vector<int>arr1,arr2;
         int n=nums.size();
-        if(n==1) return nums[0];
-        for(int i=0;i<n;i++)
-        {
-            if(i!=0) arr1.push_back(nums[i]);
-            if(i!=n-1) arr2.push_back(nums[i]);
-        }
-        return max(rob1(arr1),rob1(arr2));
+        if(n==0) return 0;       // ✨✨Don't Forget to add these 2 base comdition
+        if(n==1) return nums[0]; // ✨✨Don't Forget to add these 2 base comdition
+        vector<int>dp1(n,-1);
+        vector<int>dp2(n,-1);
+        
+        int take_0th_el=solve(0,n-2,nums,dp1); // If u are taking 0th element , then u have to send here n-2 as the end index
+        int Not_Take_0th_el=solve(1,n-1,nums,dp2);  // If u are ignoring the first index, then u can take the last index
+        return max(take_0th_el,Not_Take_0th_el);
     }
 };
