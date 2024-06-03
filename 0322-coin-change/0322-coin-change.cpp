14981430cc7f755e8201199cc43315d9c6299675
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-06-03 13:00:42 +0530

    Time: 40 ms (41.38%), Space: 20.2 MB (39.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,18 @@
-// Tabulation
+// Space Optimization
 // TC=O(n*sum)
-// SC=O(n*sum)
+// SC=O(sum)
 
 class Solution {
 public:
     
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,INT_MAX-100));
+        vector<int>prev(amount+1,INT_MAX-100);
+        vector<int>curr(amount+1,INT_MAX-100);
         
         for(int j=0;j<=amount;j++)
         {
-            if ((j%coins[0])==0) dp[0][j] = j/coins[0]; //⭐⭐Mistake , here u did wrong and instead of j , u  used the amount variable.
+            if ((j%coins[0])==0) prev[j] = j/coins[0]; //⭐⭐Mistake , here u did wrong and instead of j , u  used the amount variable.
 //             so pls use the jth variable.
         }
         
@@ -19,18 +20,20 @@ public:
         {
             for(int j=0;j<=amount;j++)
             {
-                int nottake=dp[ind-1][j];
+                int nottake=prev[j];
                 int take=INT_MAX-100;
                 if(j>=coins[ind])
                 {
-                  take=1+dp[ind][j-coins[ind]];
+                  take=1+curr[j-coins[ind]];
                  } 
                 
-                dp[ind][j]=min(take,nottake);
+                curr[j]=min(take,nottake);
+                
             }
+            prev=curr;
         }
         
-        int ans=dp[n-1][amount];
+        int ans=prev[amount];
         
         if(ans==INT_MAX-100) return -1; // ⭐⭐Edge case, when ans is -1 return INT_MAX-100
         else return ans;
