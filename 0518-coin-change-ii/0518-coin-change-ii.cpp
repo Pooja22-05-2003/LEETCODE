431248cc6b51adb6d7921c7d725f1c6b77fd2e95
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-06-03 16:30:48 +0530

    Time: 10 ms (55.51%), Space: 8.7 MB (79.53%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,4 +1,4 @@
-// Tabulation
+// Space Optimization
 // TC>=O(n*amount)
 // SC=O(n*amount) 
 class Solution {
@@ -6,28 +6,30 @@ public:
  
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        vector<int>prev(amount+1,0);
+        vector<int>curr(amount+1,0);
         if(amount==0) return 1; // edge case
         
         for(int j=0;j<=amount;j++)
         {
-            if(j%coins[0]==0) dp[0][j]=1;
+            if(j%coins[0]==0) prev[j]=1;
         }
         
         for(int ind=1;ind<n;ind++)
         {
             for(int j=0;j<=amount;j++)
             {
-                int nottake=dp[ind-1][j];
+                int nottake=prev[j];
                 int take=0;
                 if(coins[ind]<=j)
                 {
-                   take=dp[ind][j-coins[ind]];
+                   take=curr[j-coins[ind]];
                 }
         
-              dp[ind][j]=take+nottake;
+              curr[j]=take+nottake;
             }
+            prev=curr;
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 };
