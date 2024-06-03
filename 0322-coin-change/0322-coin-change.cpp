602a7d02d2009078af00fa9dbff0dcceecd4c7d1
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-06-03 12:57:54 +0530

    Time: 33 ms (53.63%), Space: 46.9 MB (28.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,36 +1,35 @@
-// Memoization
+// Tabulation
 // TC=O(n*sum)
 // SC=O(n*sum)
 
 class Solution {
 public:
-    int solve(int ind, int target, vector<int>&coins, vector<vector<int>>&dp)
-    {
-        //⭐⭐Unique edge cases, different from knapsack problems
-//         Here only when we reach the last ind we will return something
-        
-        // ⭐⭐Must return INT_MAX-100
-        if(ind==0)
-        {
-        if ((target%coins[ind])==0) return target/coins[ind];
-        return INT_MAX-100;
-        }
-       
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        int nottake=solve(ind-1,target,coins,dp);
-        int take=INT_MAX-100;
-        if(target>=coins[ind])
-        {
-            take=1+solve(ind,target-coins[ind],coins,dp);
-        }
-        
-        // cout<<"ind:"<<ind<<" take:"<<take<<" nottake:"<<nottake<<endl;
-        return dp[ind][target]=min(take,nottake);
-    }
+    
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=solve(n-1,amount,coins,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,INT_MAX-100));
+        
+        for(int j=0;j<=amount;j++)
+        {
+            if ((j%coins[0])==0) dp[0][j] = j/coins[0];
+        }
+        
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int j=0;j<=amount;j++)
+            {
+                int nottake=dp[ind-1][j];
+                int take=INT_MAX-100;
+                if(j>=coins[ind])
+                {
+                  take=1+dp[ind][j-coins[ind]];
+                 } 
+                
+                dp[ind][j]=min(take,nottake);
+            }
+        }
+        
+        int ans=dp[n-1][amount];
         
         if(ans==INT_MAX-100) return -1; // ⭐⭐Edge case, when ans is -1 return INT_MAX-100
         else return ans;
