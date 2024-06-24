9f72d2a65f53ce80c6fc22f9fc0cb343084aeadc
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-06-24 16:23:05 +0530

    Time: 5 ms (13.09%), Space: 8.3 MB (58.00%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,29 +1,30 @@
-// Power Set
-// TC=O(2^n*n)
-// SC=O(1)
-
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> ans;
-        vector<int> temp;
-
-        // Outer loop will run from 0 -> (2^n)
-        int pow_size=pow(2,nums.size());
-        for(int i=0;i<pow_size;i++){
-            // Inner loop will run from 0-> n
-            for(int j=0;j<n;j++){
-                // if the ith bit is set , then take the ele that index present in the array
-                // For checking the ith bit is set or not, do the left shift of j=> (1<<j)
-                if(i & (1<<j)){
-                    temp.push_back(nums[j]);
+        vector<vector<int>>ans;
+       
+        
+        // ans.push_back(arr);
+        
+        int end=pow(2,n);
+        
+        for(int i=0;i<end;i++)
+        {
+            
+             vector<int>arr;
+            for(int j=0;j<30;j++)
+            {
+                if((i>>j)&1)
+                {
+                    arr.push_back(nums[j]);
                 }
             }
-            ans.push_back(temp);
-            temp.clear();
+            
+            ans.push_back(arr);
+            
         }
-
+        
         return ans;
     }
 };
