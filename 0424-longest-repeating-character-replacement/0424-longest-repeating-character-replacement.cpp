db4255e083002e2f4515c41badf8521912916958
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-06-28 15:43:44 +0530

    Time: 20 ms (12.51%), Space: 8.2 MB (59.09%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int>mp(255,0);
+        int n=s.size();
+        int i=0;
+        int j=0;
+        int res=0;
+        int maxlen=INT_MIN;
+        while(j<n)
+        {
+              mp[s[j]]++;
+            
+              maxlen=max(maxlen,mp[s[j]]);
+              // cout<<"j:"<<j<<" maxlen:"<<maxlen<<endl;
+            
+            // window is from i to j
+            
+            
+                while(((j-i+1)-maxlen)>k && j<n)
+                {
+                    // cout<<"jj:"<<j<<" ii:"<<i<<endl;
+                    mp[s[i]]--;
+                    i++;
+                    for(int k=0;k<255;k++)
+                    {
+                        maxlen=max(maxlen,mp[k]);
+                    }
+                }
+        
+                res=max(res,((j-i+1)));
+                // cout<<"j:"<<j<<" res:"<<res<<endl;
+                j++;
+            
+        }
+        
+        return res;
+    }
+    
+    
+    /*
+    A  B  A  B
+    0  1  2  3
+    i
+          j
+    
+    maxlen=2 , res=3
+    
+    
+    arr A=2,
+        B=2,
+    
+    
+    */
+};
