4dcf95a20c0f1d79554669c50ecea16c3994b2ff
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-06-28 09:30:40 +0530

    Time: 3 ms (95.56%), Space: 10.4 MB (77.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,37 +5,26 @@ public:
         if(n==0) return 0; // edge case
         // if(s==" " ) return 1;
         int maxlen=1;
-        unordered_map<char,int>mp;
+        vector<int>mp(255,-1);
         int i=0;
+        int j=0;
         // int len=0;
-        while(i<n)
+        while(j<n)
         { 
-            mp[s[i]]=i;
-            // len++;
-            
-            int j=i+1;
-            while(j<n)
+            if(mp[s[j]]>=i)
             {
-                if(mp.find(s[j])!=mp.end())
-                {
-                    // cout<<"i:"<<i<<" j:"<<j<<endl;
-                    i=mp[s[j]];
-                    mp.clear();
-                    // i=j-1;
-                    // len--;
-              
-                    break;
-                    
-                }
-                 // len++;
-                maxlen=max(maxlen,j-i+1);// i to j-1 is valid
-                
-                mp[s[j]]=j;
+                i=mp[s[j]]+1;
                
-                j++;
+                mp[s[j]]=j;
+                 j++;
+                continue;
+               
             }
-            cout<<"i:"<<i<<endl;
-            i++;
+            
+            maxlen=max(maxlen,(j-i+1));
+            mp[s[j]]=j;
+           
+            j++;
         }
         
         return maxlen;
