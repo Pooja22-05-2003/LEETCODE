5725d3048448919cadec7cc9121e5b70e9ec2ff4
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-06-28 11:29:16 +0530

    Time: 47 ms (25.64%), Space: 57.8 MB (48.81%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,40 +1,15 @@
-// TC=O(N)+O(N)=O(2N)=O(N)
+// TC=O(N)
 // SC=O(1)
 
+
+// Eliminating the k loop.
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         int n=nums.size();
         int len=0;
-        // for(int i=0;i<n;i++)
-        // {
-        //     int cnt0=0;
-        //     for(int j=i;j<n;j++)
-        //     {
-        //         if(nums[j]==0) cnt0++;
-        //         if(cnt0>k) break;
-        //         len=max(len,j-i+1);
-        //     }
-        // }
-        
-        if(k==0) // edge case
-        {
-            int maxlen=0;
-            int len=0;
-            for(int i=0;i<n;i++)
-            {
-                if(nums[i]==1) len++;
-                else 
-                {
-                    maxlen=max(maxlen,len);
-                    len=0;
-                }
-                
-            }
-            maxlen=max(maxlen,len);
-            return maxlen;
-        }
-        
+       
+     
         int i=0;
         int j=0;
         int cnt0=0;
@@ -43,15 +18,14 @@ public:
         {
             if(nums[j]==0) cnt0++;
             
-            while(i<j && cnt0>k)
+            if(cnt0>k)
             {
-                if(nums[i]==0)
-                {
-                    cnt0--;
-                }
+                if(nums[i]==0) cnt0--;
                 i++;
             }
+            if(cnt0<=k){
             len=max(len,j-i+1);
+            }
             j++;
         }
         
