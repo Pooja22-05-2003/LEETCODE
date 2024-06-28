162fbf955382d3a3b13f9becf34729563ce6fa1a
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-06-28 09:31:33 +0530

    Time: 6 ms (84.50%), Space: 10.3 MB (77.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,7 +5,7 @@ public:
         if(n==0) return 0; // edge case
         // if(s==" " ) return 1;
         int maxlen=1;
-        vector<int>mp(255,-1);
+        vector<int>mp(255,-1); // We are using array and not map, because map will initlize all the char value to 0, and then it will give wrong ans for test case s="au"
         int i=0;
         int j=0;
         // int len=0;
