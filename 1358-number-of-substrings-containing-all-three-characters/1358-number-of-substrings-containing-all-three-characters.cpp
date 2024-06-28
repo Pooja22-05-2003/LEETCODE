34a681895d9aa0eb31b9ca1f56be7d2f1ec0c5f9
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-06-28 17:48:28 +0530

    Time: 14 ms (83.42%), Space: 9.7 MB (44.64%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,70 @@
+
+class Solution {
+public:
+    bool valid(int a, int b, int c) {
+        return (a > 0 && b > 0 && c > 0);
+    }
+
+    int numberOfSubstrings(string s) {
+        int cnta = 0, cntb = 0, cntc = 0;
+        int n = s.size();
+        int res = 0;
+        int i = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (s[j] == 'a') cnta++;
+            else if (s[j] == 'b') cntb++;
+            else if (s[j] == 'c') cntc++;
+
+            while (valid(cnta, cntb, cntc)) {
+                res += (n - j); // All substrings from i to n are valid when valid(cnta, cntb, cntc) is true
+                if (s[i] == 'a') cnta--;
+                else if (s[i] == 'b') cntb--;
+                else if (s[i] == 'c') cntc--;
+                i++;
+            }
+        }
+
+        return res;
+    }
+};
+/*
+class Solution {
+public:
+    bool valid(int a, int b,int c)
+    {
+        return ((a>0 && b>0) && c>0)?true:false;
+    }
+    int numberOfSubstrings(string s) {
+        int cnta=0;
+        int cntb=0;
+        int cntc=0;
+        int n=s.size();
+        int extra=0;
+        int i=0;
+        int j=0;
+        int res=0;
+        while(j<n)
+        {
+             if(s[j]=='a') cnta++;
+             else if(s[j]=='b') cntb++;
+             else cntc++;
+                while((i<j) && (valid(cnta,cntb,cntc)) )
+                {
+                    if(s[i]=='a') cnta--;
+                    else if(s[i]=='b') cntb--;
+                    else cntc--;
+
+                    i++;
+                    if(valid(cnta,cntb,cntc)==true) extra++;
+                    else extra=0;
+                }
+            
+            if((valid(cnta,cntb,cntc))) res=res+1+extra;
+            j++;
+        }
+        
+        return res;
+    }
+};
+*/
