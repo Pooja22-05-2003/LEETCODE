3257d5bcb2449c5d17abceee7c59083ca5aa42f0
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-06-29 12:03:57 +0530

    Time: 124 ms (61.37%), Space: 55.8 MB (69.56%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,34 @@
+// Approach1:
+// TC=2*(2*n)==O(n)
+// SC=O(n)
+class Solution {
+public:
+    int solve(vector<int>&nums, int k)
+    {
+        int res=0;
+        int i=0;
+        int j=0;
+        int n=nums.size();
+        
+        unordered_map<int,int>mp;
+        
+        while(j<n)
+        {
+            mp[nums[j]]++;
+            
+            while(mp.size()>k)
+            {
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0) mp.erase(nums[i]);
+                i++;
+            }
+            res+=(j-i+1);  // it will store all the subarray of size <=k
+            j++;
+        }
+        
+        return res;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return solve(nums,k)-solve(nums,k-1);
+    }
+};
