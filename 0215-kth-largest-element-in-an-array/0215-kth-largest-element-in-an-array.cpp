85c430007c0363cf090c2fcb2fd0d7d0921edaa8
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-01 08:55:41 +0530

    Time: 115 ms (13.41%), Space: 64.2 MB (22.14%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,3 +1,9 @@
+// If we maintain always the size of the PQ to be of size k.
+// whenever size exceed k, pop the smallest element.[maintain min-heap, it will store min element at the top.]
+// Now after iterating the whole array, the kth largest element is at the top of the priority queue of k size, so return pq.top
+
+// TC=O(N*logk)
+// SC=O(k)
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
