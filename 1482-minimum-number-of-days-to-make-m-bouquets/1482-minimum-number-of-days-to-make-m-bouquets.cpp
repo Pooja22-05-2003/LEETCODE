7eb5827e5a066ba54709980424ad00c5922b106a
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-04 01:05:19 +0530

    Time: 145 ms (13.69%), Space: 69.1 MB (26.84%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,46 +1,88 @@
+// TC=O(n*log(10^9))
 class Solution {
 public:
-
-    int getNumOfBouquets(vector<int>& bloomDay, int mid, int k) {
-        int numOfBouquets = 0;
-
-        int count = 0;
-        for (int i = 0; i < bloomDay.size(); i++) {
-         
-            if (bloomDay[i] <= mid) {
-                count++;
-            } else {
-                count = 0;
+    bool check(int day, int m , int k , vector<int>&bloomDay)
+    {
+        // lets take the  k size window
+        
+        int win=0;
+        int cnt=0;
+        
+        for(int i=0;i<bloomDay.size();i++)
+        {
+            if(bloomDay[i]<=day) {
+                win++;
+                
+                if(win==k){
+                    cnt++;
+                    win=0;
+                }
+                
+                
             }
-
-            if (count == k) {
-                numOfBouquets++;
-                count = 0;
+            
+            else{
+                win=0;
             }
+            
+            if(cnt>m) break;
+            
+            
         }
-
-        return numOfBouquets;
+        
+        // cout<<"day:"<<day<<" cnt:"<<cnt<<endl;
+        if(cnt>=m) return true;
+        else return false;
+        
     }
-
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int start = 0;
-        int end = 0;
-        for (int day : bloomDay) {
-            end = max(end, day);
+        int n=bloomDay.size();
+        // long long  flowers_needed=(m*k);
+        // if(flowers_needed>n) return -1;
+        
+        int maxx=*max_element(bloomDay.begin(),bloomDay.end());
+        // for(int day=1;day<=maxx;day++)
+        // {
+        //     if(check(day,m,k,bloomDay)==true) return day;
+        //     // cout<<"day_inside:"<<day<<endl;
+        // }
+        
+        int st=1;
+        int end=maxx;
+        
+        while(st<=end)
+        {
+            int mid=(st+end)/2;
+            
+            if(mid-1==0 && check(mid,m,k,bloomDay)==true) return mid;
+            if((check(mid,m,k,bloomDay)==true) && check(mid-1,m,k,bloomDay)==false) return mid;
+            else if(check(mid,m,k,bloomDay)==false) st=mid+1;
+            else end=mid-1;
         }
-
-        int minDays = -1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
-            if (getNumOfBouquets(bloomDay, mid, k) >= m) {
-                minDays = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-
-        return minDays;
+        
+        return -1;
+        
     }
 };
+
+/*
+Edge case:
+flowes_needed=m*k
+if(n<flowers_needed) return -1
+
+
+t=1;t<=max_el
+
+check()
+
+{
+k size window and count the maximum bouqet i can create till the ith day of blooming.
+
+if(Cnt>=m) return true
+else return false
+}
+
+
+
+
+*/
