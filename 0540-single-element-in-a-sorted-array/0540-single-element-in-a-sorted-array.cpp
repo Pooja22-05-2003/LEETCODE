b066d85ad3ee50dfe6011249887bac6c8f6eece1
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-03 10:58:23 +0530

    Time: 25 ms (14.26%), Space: 24.7 MB (14.00%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int n=arr.size();
+        
+        int st=0;
+        int end=n-1;
+        
+        if(n==1) return arr[0];
+        if(st+1<n && arr[0]!=arr[1]) return arr[0];
+        else if(end-1>=0 && arr[n-1]-arr[n-2]) return arr[n-1];
+        
+        while(st<=end)
+        {
+            int mid=(st+end)/2;
+            bool isEven = (end-mid)%2==0; // if right part of the arr after mid is even or not.
+            
+            if(arr[mid] == arr[mid+1]) {
+                if(isEven) {              // if the arr[mid]==arr[mid+1], then now the right part contain the odd number of elements., neglect the left part and move right to st=mid+2
+                    st = mid+2;
+                } else {
+                    end = mid-1;
+                }
+            } else if(arr[mid] == arr[mid-1]) {
+                if(isEven) {
+                    end = mid-2;
+                } else {
+                    st = mid+1;
+                }
+            } else {
+                return arr[mid];
+            }
+        }
+    
+    return arr[st];
+            
+    }
+};
