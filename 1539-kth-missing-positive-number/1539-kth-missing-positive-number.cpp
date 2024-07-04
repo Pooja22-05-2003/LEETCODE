69b3f2fd0b6c312b1d73d9d4e101c59b4809610c
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-04 14:14:48 +0530

    Time: 2 ms (75.04%), Space: 12 MB (41.71%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -6,6 +6,7 @@ public:
          int high=n-1;
          int mid=-1;
          while(low<=high){
+             // cout<<"low:"<<low<<" high:"<<high<<" mid:"<<low+(high-low)/2<<endl;
              mid=low+(high-low)/2;
              int x=arr[mid]-(mid+1);
              if(x<k){
@@ -15,6 +16,13 @@ public:
                  high=mid-1;
              }
          }
-       return low+k;
+        // cout<<"low:"<<low<<endl;
+       return low+k; 
+        /*
+        arr = [1,2,3,4], k = 2
+        
+        when low=4  then  while loop will break and we need to return 2nd element after 3rd index(meand element=4 is present at the third index) return 4+2=6.
+        
+        */
     }
 };
