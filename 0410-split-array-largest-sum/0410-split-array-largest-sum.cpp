60524a7c7eea51bfd596fa0268146b03e730724f
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-05 08:56:39 +0530

    Time: 4 ms (40.87%), Space: 9.1 MB (71.68%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,67 @@
+// O(nlog(sum))
+class Solution {
+public:
+    bool check(int sum, vector<int>&nums, int k)
+    {
+        int cnt=1;
+        if(nums[0]>sum) return false;//////////
+        int s=nums[0];
+        
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]>sum) return false;//////////
+            if((nums[i]+s)<=sum)
+            {
+                s+=nums[i];
+            
+            }
+            else
+            {
+                s=0;
+                cnt++;
+                s+=nums[i];
+            }
+        }
+        
+        // cout<<"sum:"<<sum<<" cnt:"<<cnt<<endl;
+        if(cnt<=k) return true;  // I used >=k , but it is incorrect, correction condition is (cnt<=k)
+        else return false;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int minEl=INT_MAX;
+        int maxEl=INT_MIN;
+        int sum=0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            minEl=min(minEl,nums[i]);
+            maxEl=max(maxEl,nums[i]);
+            sum+=nums[i];
+        }
+        
+        // cout<<"minEl:"<<minEl<<" maxEl:"<<maxEl<<" sum:"<<sum<<endl;
+//         for(int i=minEl;i<=sum;i++)
+//         {
+//             // cout<<"i:"<<i<<endl;
+//             bool temp=check(i,nums,k);
+//             // cout<<"temp:"<<temp<<endl;
+//             if(check(i,nums,k)==true) return i;
+
+//         }
+        
+        
+        int st=minEl;
+        int end=sum;
+        
+        while(st<=end)
+        {
+            int mid=(st+end)/2;
+            
+            if(mid-1==0 && check(mid,nums,k)==true) return mid;
+            if(check(mid,nums,k)==true && check(mid-1,nums,k)==false) return mid;
+            if(check(mid,nums,k)==false) st=mid+1;
+            else end=mid-1;
+        }
+        return -1;
+    }
+};
