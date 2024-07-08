dcd6cae549e8b3194a1c72f1ae9cb83cb8f11c61
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-08 20:35:42 +0530

    Time: 350 ms (58.26%), Space: 177.8 MB (44.30%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,57 @@
+class LRUCache {
+public:
+     list<int>dll;
+    unordered_map<int,pair<list<int>::iterator,int>>mp; // this map will store the key->address and the value
+    int n;
+    LRUCache(int capacity) {
+        n=capacity;
+    }
+    
+    void makeRecentlyUsed(int key)
+    {
+        dll.erase(mp[key].first);
+        dll.push_front(key);
+        mp[key].first=dll.begin();
+    }
+    
+    int get(int key) {
+    
+        if(mp.find(key)!=mp.end())
+        {
+            makeRecentlyUsed(key);
+            return mp[key].second;
+        }
+        
+        return -1;
+        
+    }
+    
+    void put(int key, int value) {
+        
+        if(mp.find(key)!=mp.end())
+        {
+            mp[key].second=value;
+            makeRecentlyUsed(key);
+        }
+        else
+        {
+            dll.push_front(key);
+            mp[key]={dll.begin(),value};
+            n--;
+        }
+        
+        while(n<0)
+        {
+            mp.erase(dll.back());
+            dll.pop_back();
+            n++;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
