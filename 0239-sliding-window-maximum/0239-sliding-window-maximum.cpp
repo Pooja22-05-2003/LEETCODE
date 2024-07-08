6c9336650619193b973c77ece641842fd591cd1c
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-08 17:25:20 +0530

    Time: 233 ms (21.40%), Space: 158.7 MB (16.41%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,30 @@
+// Approach2: Better Approach
+// TC=O(n*log(n))
+// SC=O(n)
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        int n=nums.size();
+        vector<int>res;
+        // element,ind
+        priority_queue<pair<int,int>>pq;
+        
+        for(int i=0;i<=k-2;i++) pq.push({nums[i],i});
+        
+        for(int i=0;i<n;i++)
+        {
+            // we cannot tolerated the topmost element index out of the window {valid window size[(i-k-1).....i]}
+            pq.push({nums[i],i});
+            if(i>=(k-1)){
+            while(!pq.empty() && pq.top().second<=(i-k)) pq.pop();
+            
+            res.push_back(pq.top().first);
+            }
+            
+           
+        }
+        
+        return res;
+    }
+};
