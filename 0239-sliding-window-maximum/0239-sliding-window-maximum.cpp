72e92a189fa7fdbd60eec7ec1ede2e11988f21e6
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-08 17:29:25 +0530

    Time: 205 ms (28.17%), Space: 152.3 MB (25.60%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -10,14 +10,14 @@ public:
         // element,ind
         priority_queue<pair<int,int>>pq;
         
-        for(int i=0;i<=k-2;i++) pq.push({nums[i],i});
+     
         
         for(int i=0;i<n;i++)
         {
             // we cannot tolerated the topmost element index out of the window {valid window size[(i-k-1).....i]}
             pq.push({nums[i],i});
             if(i>=(k-1)){
-            while(!pq.empty() && pq.top().second<=(i-k)) pq.pop();
+            while(!pq.empty() && pq.top().second<=(i-k)) pq.pop(); // ⭐here i missed the equal to sign in pq.top.second<=.
             
             res.push_back(pq.top().first);
             }
