eb0741ae1a20d82abb641bd83a04a8ad3fd52dcc
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-08 10:51:25 +0530

    Time: 8 ms (81.81%), Space: 12 MB (9.94%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,51 +1,36 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        
-        // edge case
         if(num.size()==k) return "0";
-        string res="";
-        int n=num.size();
-        stack<int>st;
-        // puhs the first element in the stack
         
-        for(int i=0;i<n;i++)
+        stack<int>st;
+        
+        for(int i=0;i<num.size();i++)
         {
-            while(!st.empty() && st.top()>num[i] && k>0)
-            { 
-                cout<<"hi"<<endl;
+            while(st.size()>0 && st.top()>num[i] && k>0)
+            {
                 st.pop();
                 k--;
             }
             
-             if(st.size() > 0 || num[i] != '0') {
-                st.push(num[i]); //to avoid the case when we have preceeding zeros
-            }
+            // push only of the number is non zero, or if it is zero, then it is not preceding zero
+            if(!st.empty() || num[i]!='0') st.push(num[i]);
         }
         
-        // remove the preceding zeroes
-        
-       
-        
-        // creating the final ans res
-        while(st.size()>0 && k>0)
-        {
-            
+        while(!st.empty() && k>0){
             st.pop();
             k--;
         }
-            
+        
+        string res="";
         
         while(!st.empty())
         {
-         
             res.push_back(st.top());
             st.pop();
-          
         }
         
         reverse(res.begin(),res.end());
-        return (res=="")?"0":res; // This is the edge case["10"
-                                                        //  1], here output is "0" and not ""
+        return (res=="")?"0":res;
     }
 };
