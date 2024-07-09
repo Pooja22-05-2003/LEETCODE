a868f8709fef7ece6886b2368483992d8129cb3a
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-09 16:57:29 +0530

    Time: 10 ms (85.28%), Space: 15 MB (5.95%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,22 +1,102 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n=nums.size();
-        long long res=0;
+    
+    void nextSmallerLeft( vector<int>&nsl, vector<int>arr)
+    {
+        stack<int>st;
         
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<arr.size();i++)
         {
-            int minn=nums[i];
-            int maxx=nums[i];
+            int el=arr[i];
             
-            for(int j=i+1;j<n;j++)
-            {
-                minn=min(minn,nums[j]);
-                maxx=max(maxx,nums[j]);
-                res+=(maxx-minn);
-            }
+            while(!st.empty() && arr[st.top()]>el) st.pop(); // equal to sign will come in subarray , so no need to add equal to.
+            
+            if(st.empty()) nsl[i]=-1;
+            else nsl[i]=st.top();
+            st.push(i);
+        }
+    }
+    
+    void nextSmallerRight(vector<int>&nsr, vector<int>arr)
+    {
+        stack<int>st;
+        
+        for(int i=arr.size()-1;i>=0;i--)
+        {
+            int el=arr[i];
+            
+            while(!st.empty() && arr[st.top()]>=el) st.pop();// In this que, here equal will not be added, because we are moving in right in actual ans calcaultion , so if that element will come in right that will count in itself.
+            
+            if(st.empty()) nsr[i]=arr.size();
+            else nsr[i]=st.top();
+            st.push(i);
+        }
+    }
+    void nextGreaterLeft( vector<int>&nsl, vector<int>arr)
+    {
+        stack<int>st;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            int el=arr[i];
+            
+            while(!st.empty() && arr[st.top()]<el) st.pop(); // equal to sign will come in subarray , so no need to add equal to.
+            
+            if(st.empty()) nsl[i]=-1;
+            else nsl[i]=st.top();
+            st.push(i);
+        }
+    }
+    
+    void nextGreaterRight(vector<int>&nsr, vector<int>arr)
+    {
+        stack<int>st;
+        
+        for(int i=arr.size()-1;i>=0;i--)
+        {
+            int el=arr[i];
+            
+            while(!st.empty() && arr[st.top()]<=el) st.pop();// In this que, here equal will not be added, because we are moving in right in actual ans calcaultion , so if that element will come in right that will count in itself.
+            
+            if(st.empty()) nsr[i]=arr.size();
+            else nsr[i]=st.top();
+            st.push(i);
+        }
+    }
+    long long subArrayRanges(vector<int>& arr) {
+         int n=arr.size();
+        vector<int>nsl(n);
+        vector<int>nsr(n);
+        nextSmallerLeft(nsl,arr);
+        nextSmallerRight(nsr,arr);
+      
+        
+        long long ans=0;
+        for(int i=0;i<n;i++){
+            int l=i-nsl[i];
+            int r=nsr[i]-i;
+            
+            ans=(ans+(((long long)l*r*arr[i])));
+            //ans=(ans+(l2*arr[i])%mod)%mod;
         }
         
-        return res;
+        vector<int>ngl(n);
+        vector<int>ngr(n);
+        nextGreaterLeft(ngl,arr);
+        nextGreaterRight(ngr,arr);
+      
+        
+        long long  ans1=0;
+        for(int i=0;i<n;i++){
+            int l=i-ngl[i];
+            int r=ngr[i]-i;
+            
+            ans1=(ans1+(((long long)l*r*arr[i])));
+            //ans=(ans+(l2*arr i])%mod)%mod;
+        }
+        
+        return (ans1)-(ans);
+        
+        
     }
 };
