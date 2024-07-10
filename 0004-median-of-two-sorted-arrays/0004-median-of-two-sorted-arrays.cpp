e2d84d082f43e66d78a964fbaca965656f96af6c
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-10 18:19:06 +0530

    Time: 27 ms (48.86%), Space: 94.3 MB (89.79%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,53 +1,46 @@
-// TC=O(n+m)
-// SC=O(n+m)
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int size1=nums1.size();
-        int size2=nums2.size();
-        int i=0;
-        int j=0;
-        
-        vector<int>arr;
-        while(i<size1 && j<size2)
-        {
-            if(nums1[i]<=nums2[j])
-            {
-                arr.push_back(nums1[i]);
-                i++;
+        int n1=nums1.size();
+        int n2=nums2.size();
+        if(n1>n2){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int n=n1+n2;
+        int left=(n1+n2+1)/2;
+        int l=0; int r=n1;
+        while(l<=r){
+            int mid1=(l+r)/2;
+            int mid2=left-mid1;
+            int l1=INT_MIN; int l2=INT_MIN;
+            int r1=INT_MAX; int r2=INT_MAX;
+            if(mid1-1>=0){
+                l1=nums1[mid1-1];
             }
-            else 
-            {
-                arr.push_back(nums2[j]);
-                j++;
+            if(mid2-1>=0){
+                l2=nums2[mid2-1];
+            }
+            if(mid1<n1){
+            r1=nums1[mid1];
+            }
+            if(mid2<n2){
+            r2=nums2[mid2];
+            }
+            if(l1<=r2 and l2<=r1){
+                if((n1+n2)%2==0){
+                return ((double)max(l1,l2)+(double)min(r1,r2))/2.0;
+                }
+                else{
+                 return max(l1,l2);
+                }
+            }
+            else if(l1>r2){
+                r=mid1-1;
+            }
+            else{
+                l=mid1+1;
             }
         }
-        
-        while(i<size1)
-        {
-            arr.push_back(nums1[i]);
-            i++;
-        }
-        
-        while(j<size2)
-        {
-            arr.push_back(nums2[j]);
-            j++;
-        }
-        
-        int tot=size1+size2;
-        
-        if(tot%2==0)
-        {
-            int ind1=(tot/2)-1;
-            int ind2=tot/2;
-            return (double(arr[ind1])+(double)arr[ind2])/2;
-        }
-        else
-        {
-            return (double)arr[tot/2];
-        }
-        
-        return -1;
+        return 0;
     }
 };
