c249117e9dd251f568688e9893a00c2a533c940f
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-16 19:56:22 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (6.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int solve(int ind, vector<int>&nums,vector<int>&dp)
-    {
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int opt1=nums[ind]+solve(ind-2,nums,dp);
-        int opt2=solve(ind-1,nums,dp);
-        
-        return dp[ind]=max(opt1,opt2);
-    }
+   
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return solve(nums.size()-1,nums,dp);
+        vector<int>dp(nums.size(),0);
+        
+        dp[0]=nums[0];
+        if(nums.size()>1) dp[1]=max(nums[0],nums[1]);
+        
+        for(int ind=2;ind<nums.size();ind++)
+        {
+            int opt1=INT_MIN;
+            int opt2=INT_MIN;
+            if(ind-2>=0) opt1=nums[ind]+dp[ind-2];
+            if(ind-1>=0) opt2=dp[ind-1];
+            dp[ind]=max(opt1,opt2);
+        }
+        
+        return dp[nums.size()-1];
+        
     }
 };
