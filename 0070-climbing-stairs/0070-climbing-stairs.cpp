cd66bb67265cfcd2fba9aa64577a332b4bf91fd8
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-16 10:59:28 +0530

    Time: 3 ms (29.57%), Space: 7.6 MB (6.95%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,13 @@
-/*
-4th Approach 
-Using Space Optimization Tabulation
-
-// TC=O(n)
-// SC=O(1)
-*/
 class Solution {
 public:
-  
+    int solve(int n,vector<int>&dp)
+    {
+      if(n==1 || n==0) return 1;
+      if(dp[n]!=-1) return dp[n];
+      return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    }
     int climbStairs(int n) {
       vector<int>dp(n+1,-1);
-      int prev1=1;
-      int prev2=1;
-     
-      for(int i=2;i<=n;i++)
-      {
-          int curr=prev1+prev2;
-          prev2=prev1;
-          prev1=curr;
-      }
-      return prev1;
+      return solve(n,dp);
     }
 };
