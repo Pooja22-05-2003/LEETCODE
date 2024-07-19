9a285ab90bd8c384855bad4e9cfb9ab98680ab62
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-19 09:45:24 +0530

    Time: 0 ms (100.00%), Space: 17.8 MB (7.11%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -9,21 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// TC=O(n)
-// SC=O(n)
-
 class Solution {
 public:
-    
-    int depth(TreeNode* root)
+    int h(TreeNode* root)
     {
         if(root==NULL) return 0;
         
-        return 1+ max(depth(root->left),depth(root->right));
+        return 1+max(h(root->left),h(root->right));
     }
     int maxDepth(TreeNode* root) {
         if(root==NULL) return 0;
-        
-        return depth(root);
+        return 1+max(h(root->left),h(root->right));
     }
 };
