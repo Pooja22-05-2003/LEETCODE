5a08662dd3e2b9392e64669ae9e9f70407ef6067
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-29 11:15:44 +0530

    Time: 24 ms (36.31%), Space: 31.6 MB (26.97%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,29 @@
+/*
+Amazon-4,Mathworks-3,Google-2
+*/
+class Solution {
+public:
+   
+    int numDistinct(string s, string t) {
+        int n1=s.size();
+        int n2=t.size();
+        vector<vector<double>>dp(n1+1,vector<double>(n2+1,0));
+        
+        
+        // right shifting , because base case consider <0
+        
+        // for any index i, if the j is at 0 then we return 1
+        for(int i=0;i<=n1;i++) dp[i][0]=1;
+        
+        for(int i=1;i<=n1;i++)
+        {
+            for(int j=1;j<=n2;j++)
+            {
+                   
+                    if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                    else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return (int)dp[n1][n2];
+    }
+};
