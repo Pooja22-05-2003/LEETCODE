4208817a1185ab61af44bbf632630a7dd58aeabf
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-07-29 21:45:13 +0530

    Time: 67 ms (69.35%), Space: 13.2 MB (46.92%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,17 @@
-// Tabulation
-// TC=O(n*n)
-// SC=O(n)
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,1);
-
-        for(int i=0;i<n;i++)
+        int maxlen=1;
+        for(int i=1;i<n;i++)
         {
-            for(int j=i-1;j>=0;j--)
-            {
-                if(nums[j]<nums[i])
-                {
-                    dp[i]=max(dp[i], (1+dp[j]));
-                }
+            for(int j=i-1;j>=0;j--){
+               if(nums[i]>nums[j]) dp[i]=max(dp[i],1+dp[j]);
             }
+            maxlen=max(maxlen,dp[i]);
         }
-
-        return *max_element(dp.begin(),dp.end());
+        
+        return maxlen;
     }
 };
