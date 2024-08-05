969a9d0cb199e18f34f7d41c1294d0418d6a9981
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-05 18:38:16 +0530

    Time: 21 ms (66.72%), Space: 26.8 MB (41.03%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,12 +1,14 @@
+// TC=O(4n)
+// SC=O(2n)+O(n)// n is for res
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& arr) {
         int n=arr.size();
         vector<int>res(n,-1);
         stack<int>st;
-        for(int i=(2*n-1);i>=0;i--)
+        for(int i=(2*n-1);i>=0;i--) // O(2n)
         {
-            while(!st.empty() && arr[i%n]>=st.top()) st.pop();
+            while(!st.empty() && arr[i%n]>=st.top()) st.pop();//O(2n)
             
             if(i<n)
             {
