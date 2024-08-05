67edd25fc7d5de0c6b30816e7e145e2473dd79bb
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-05 21:22:31 +0530

    Time: 12 ms (39.07%), Space: 20.3 MB (83.00%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,5 +1,3 @@
-// TC=O(2*n)
-// SC=O(1)
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
@@ -7,37 +5,30 @@ public:
         
         for(auto el:asteroids)
         {
-            
-            while(!st.empty() && st[st.size()-1]>0 && el<0) // collision will only happen if the current esteroid is moving left and the topmost element of the stack is moving right ----> <----
-                        
+            while(!st.empty() && st[st.size()-1]>0 && el<0)
             {
-                 int sum=el+st[st.size()-1];
-                 if(sum>0)
-                 {
-                     // means the right asteroid is more powerful
-                     // do nothing
-                     el=0;
-                 }
+                int sum=st[st.size()-1]+el;
+                
+                if(sum>0)
+                {
+                    // means right asteroid is more powerful
+                    el=0;
+                }
                 else if(sum<0)
                 {
-                    // means the left is more poweful and it will kill right , so st.pop
-                    st.pop_back();                   
-                    
+                    st.pop_back();
                 }
                 else
                 {
-                    // equal
+                    // means both the left and right asteroid are of same strength
                     st.pop_back();
                     el=0;
                 }
-                
-                    
             }
             
-           if(el!=0) st.push_back(el);
+            if(el!=0) st.push_back(el);
         }
         
         return st;
-       
     }
 };
