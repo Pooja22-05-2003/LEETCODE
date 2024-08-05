3cdfc53d08e880476c22539ec3acade00e93a7b3
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-05 18:37:33 +0530

    Time: 22 ms (61.26%), Space: 26.9 MB (25.35%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,46 +1,24 @@
-// TC=O(2*n)
-// SC=O(2*n)
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& nums) {
-        
-        int n=nums.size();
+    vector<int> nextGreaterElements(vector<int>& arr) {
+        int n=arr.size();
         vector<int>res(n,-1);
-        vector<int>arr;
-        
-        for(int i=0;i<nums.size();i++)
+        stack<int>st;
+        for(int i=(2*n-1);i>=0;i--)
         {
-            arr.push_back(nums[i]);
-        }
-        
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            arr.push_back(nums[i]);
-        }
-        
-        int totalel=(nums.size()*2)-1;
-        
-        // for(int i=0;i<arr.size();i++) cout<<arr[i]<<" ";
-        cout<<endl;
-        stack<int> st;
-        
-        for(int i=arr.size()-1;i>=0;i--)
-        {
-            if(i>=n)
+            while(!st.empty() && arr[i%n]>=st.top()) st.pop();
+            
+            if(i<n)
             {
-                while(!st.empty() && st.top()<=arr[i]) st.pop();
-            }
-            else{
-                 while(!st.empty() && st.top()<=arr[i]) st.pop();
-                 if(!st.empty()) res[i]=st.top();
+                if( st.empty())
+                {
+                    res[i]=-1;
+                }
+                else res[i]=st.top();
                 
             }
             
-            
-            st.push(arr[i]);
-            
-           
-            // cout<<"i:"<<i<<" st.size():"<<st.size()<<endl;
+            st.push(arr[i%n]);
         }
         
         return res;
