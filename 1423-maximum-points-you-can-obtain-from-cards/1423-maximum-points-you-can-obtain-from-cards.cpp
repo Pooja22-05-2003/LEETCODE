04162cab61108ff65fbfedf6569abf493b568cb4
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-06 20:22:38 +0530

    Time: 33 ms (92.19%), Space: 45 MB (24.16%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
-    int solve(vector<int>&A , int n , int k ){
-       vector<int> prefix(n+2,0);
-       vector<int> suffix(n+2,0);
-
-       for(int i=1;i<=n;i++){
-          prefix[i]=prefix[i-1]+A[i];
-          // cout<<"i:"<<i<<" prefix[i]:"<<prefix[i]<<endl;
-       }
-       for(int i=n;i>=1;i--){
-          suffix[i]=suffix[i+1]+A[i];
-          // cout<<"i:"<<i<<" suffix[i]:"<<suffix[i]<<endl;
-       }
-
-    
-       int maxSum=0;
-
-       int i=0;
-       while(i<=k){
-          int j=(n-(k-i))+1;
-           // cout<<"ii:"<<i<<" jj:"<<j<<endl;
-          int sum=prefix[i]+suffix[j];
-           // cout<<"sum:"<<sum<<endl;
-          maxSum=max(maxSum,sum);
-
-          i++;
-       }
-
-       return maxSum;
-
-
-
-}
-    int maxScore(vector<int>& a, int k) {
-        // convert the array to 1 based indexing.
-        int n=a.size();
-        vector<int>arr(a.size()+1);
-        
-        for(int i=1;i<=n;i++)
+    int maxScore(vector<int>& cardPoints, int k) {
+        int cnt=k;
+        int res=0;
+        int n=cardPoints.size();
+        int sum=0;
+        int j;
+        for( j=n-1;j>=0;j--)
         {
-            arr[i]=a[i-1];
+            if(k>0) sum+=cardPoints[j];
+            else break;
+            
+            res=max(res,sum);
+            k--;
         }
         
-        int ans=solve(arr,n,k);
-        return ans;
+        j++;
+        for(int i=0;i<cnt;i++)
+        {
+           sum-=cardPoints[j];
+           j++;
+           sum+=cardPoints[i];
+           res=max(res,sum);
+        }
+        return res;
     }
 };
