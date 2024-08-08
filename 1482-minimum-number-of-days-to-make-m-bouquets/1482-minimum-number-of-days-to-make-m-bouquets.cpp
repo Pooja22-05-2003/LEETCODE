744f7482c2c6650ef8e4cd28c8952ba9b1e05d6a
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-08 15:35:47 +0530

    Time: 136 ms (19.27%), Space: 69.4 MB (12.85%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,88 +1,55 @@
-// TC=O(n*log(10^9))
 class Solution {
 public:
-    bool check(int day, int m , int k , vector<int>&bloomDay)
+    bool check(int kk, vector<int>& arr, int m, int k)
     {
-        // lets take the  k size window
-        
-        int win=0;
         int cnt=0;
-        
-        for(int i=0;i<bloomDay.size();i++)
+        int continuos=0;
+        for(int i=0;i<arr.size();i++)
         {
-            if(bloomDay[i]<=day) {
-                win++;
-                
-                if(win==k){
-                    cnt++;
-                    win=0;
-                }
-                
-                
+            if(arr[i]<=kk) 
+            {
+                continuos++;
+                if(continuos==k)
+               {
+                    
+                cnt++;
+                continuos=0;
+               }
             }
-            
-            else{
-                win=0;
-            }
-            
-            if(cnt>m) break;
-            
+            else continuos=0;
             
         }
-        
-        // cout<<"day:"<<day<<" cnt:"<<cnt<<endl;
-        if(cnt>=m) return true;
-        else return false;
-        
+        // cout<<"i:"<<kk<<" cnt:"<<cnt<<endl;
+        return (cnt>=m);
     }
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        int n=bloomDay.size();
-        // long long  flowers_needed=(m*k);
-        // if(flowers_needed>n) return -1; //⭐⭐This condition is not needed, for large case it was goin the integer overflow in multipcaiton (10^5*10^5) is so large.
+    
+    
+    int minDays(vector<int>& arr, int m, int k) {
+        int n=arr.size();
+         // long long int temp=m*k;
+        // if((temp)>n) return -1;
+       
+        int minn=INT_MAX;
+        int maxx=INT_MIN;
         
-        int maxx=*max_element(bloomDay.begin(),bloomDay.end());
-        // for(int day=1;day<=maxx;day++)
-        // {
-        //     if(check(day,m,k,bloomDay)==true) return day;
-        //     // cout<<"day_inside:"<<day<<endl;
-        // }
+        for(auto el: arr)
+        {
+            minn=min(minn,el);
+            maxx=max(maxx,el);
+        }
         
-        int st=1;
+        int st=minn;
         int end=maxx;
         
         while(st<=end)
         {
             int mid=(st+end)/2;
-            
-            if(mid-1==0 && check(mid,m,k,bloomDay)==true) return mid;
-            if((check(mid,m,k,bloomDay)==true) && check(mid-1,m,k,bloomDay)==false) return mid;
-            else if(check(mid,m,k,bloomDay)==false) st=mid+1;
-            else end=mid-1;
-        }
-        
-        return -1;
-        
+            // cout<<"mid:"<<mid<<" st:"<<st<<" end:"<<end<<endl;
+            if(mid==1 && check(mid,arr,m,k)==true) return mid;
+            if(check(mid,arr,m,k)==true && check(mid-1,arr,m,k)==false) return mid;
+            if(check(mid,arr,m,k)==true) end=mid-1;
+            else st=mid+1;
+        }  
+        return -1;    
     }
 };
-
-/*
-Edge case:
-flowes_needed=m*k
-if(n<flowers_needed) return -1
-
-
-t=1;t<=max_el
-
-check()
-
-{
-k size window and count the maximum bouqet i can create till the ith day of blooming.
-
-if(Cnt>=m) return true
-else return false
-}
-
-
-
-
-*/
