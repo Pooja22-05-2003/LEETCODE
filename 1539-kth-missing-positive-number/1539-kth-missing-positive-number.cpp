b158e457d639ab4788e7b6d9079caf4934731bce
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-08 16:21:32 +0530

    Time: 7 ms (28.78%), Space: 12.4 MB (9.52%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-         int n=arr.size();
-         int low=0;
-         int high=n-1;
-         int mid=-1;
-         while(low<=high){
-             // cout<<"low:"<<low<<" high:"<<high<<" mid:"<<low+(high-low)/2<<endl;
-             mid=low+(high-low)/2;
-             int x=arr[mid]-(mid+1);
-             if(x<k){
-             low=mid+1;
-             }
-             else {
-                 high=mid-1;
-             }
-         }
-        // cout<<"low:"<<low<<endl;
-       return low+k; 
-        /*
-        arr = [1,2,3,4], k = 2
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]<=k)
+            {
+                k++;
+            }
+            else break;    
+                
+        }
         
-        when low=4  then  while loop will break and we need to return 2nd element after 3rd index(meand element=4 is present at the third index) return 4+2=6.
-        
-        */
+        return k;
     }
 };
