c70df742318886445e1dd5dcb92edd4fe598065c
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-08 15:47:43 +0530

    Time: 36 ms (36.16%), Space: 25 MB (34.98%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,50 +1,45 @@
-// TC=O(n(log(maxElement)))
-// SC=O(1)
 class Solution {
 public:
-    bool check(int d, vector<int>&nums,int thr)
+      bool check(int kk, vector<int>& arr, int k)
     {
         int cnt=0;
-        
-        for(int i=0;i<nums.size();i++)
+       
+        for(int i=0;i<arr.size();i++)
         {
-            // cnt+=floor(nums[i]/d);
-            if(nums[i]%d==0) cnt+=(nums[i])/d;
-            else cnt+=((nums[i])/d)+1;
+           cnt+=(arr[i]/kk)+((arr[i]%kk)!=0);
+            // cout<<"i:"<<kk<<" i:"<<i<<" cnt:"<<cnt<<endl;
+            if(cnt>k) break;
+            
         }
-        
-        
-        if(cnt<=thr) return true;
-        else return false;
+        // cout<<"i:"<<kk<<" cnt:"<<cnt<<endl;
+        return (cnt<=k);
     }
-    int smallestDivisor(vector<int>& nums, int threshold) {
-        int min_el=1;
-        int max_el=INT_MIN;
+    int smallestDivisor(vector<int>& arr, int k) {
+         int n=arr.size();
+         // long long int temp=m*k;
+        // if((temp)>n) return -1;
+       
+        int minn=INT_MAX;
+        int maxx=INT_MIN;
         
-        for(auto el:nums){
-            // min_el=min(min_el,el);
-            max_el=max(max_el,el);
+        for(auto el: arr)
+        {
+            minn=min(minn,el);
+            maxx=max(maxx,el);
         }
-        
-        // for(int i=1;i<=max_el;i++)  // ⭐⭐Here we cannot run min_value from min_el, ans can also be 1 even if it was not present in this array.
-        // {
-        //     if(check(i,nums,threshold)==true) return i;
-        // }
         
         int st=1;
-        int end=max_el; // Threshold in the constraints it is given that it will always be greater than equal to nums.size();
+        int end=maxx;
         
         while(st<=end)
         {
             int mid=(st+end)/2;
-            
-            if(mid-1==0 && check(mid,nums,threshold)==true ) return mid;
-            if(check(mid,nums,threshold)==true && check(mid-1,nums,threshold)==false) return mid;
-            
-            else if(check(mid,nums,threshold)==true) end=mid-1;
+            // cout<<"mid:"<<mid<<" st:"<<st<<" end:"<<end<<endl;
+            if(mid==1 && check(mid,arr,k)==true) return mid;
+            if(check(mid,arr,k)==true && check(mid-1,arr,k)==false) return mid;
+            if(check(mid,arr,k)==true) end=mid-1;
             else st=mid+1;
-        }
-        
-        return INT_MAX;
+        }  
+        return -1;    
     }
 };
