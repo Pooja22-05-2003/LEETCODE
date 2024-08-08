8cdff459b523617948331d9bd597bf66517328ba
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-08 22:03:32 +0530

    Time: 3 ms (56.93%), Space: 12.8 MB (36.95%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,30 +1,28 @@
-// In the binary search , we eliminate one part, it can be left or right.
-// search the part which is sorted, then compare the min of that part with the global minimum . and eliminate the part which is sorted and repeat this , until st<=end.
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        int n=nums.size();
+    int findMin(vector<int>& arr) {
         int st=0;
-        int end=nums.size()-1;
+        int end=arr.size()-1;
         int ans=INT_MAX;
-        
         while(st<=end)
         {
             int mid=(st+end)/2;
-            ans=min(ans,nums[mid]); // ⭐Must add this condition
-            // check whether left part is sorted?
-            if(nums[st]<=nums[mid]) {
-                ans=min(ans,nums[st]); //...in the sorted part minimum element is always the first element.
-                st=mid+1;
+            
+           
+            if(arr[mid]>=arr[st])
+            {
+                // left part is sorted
+               ans=min(ans,arr[st]);
+              st=mid+1;
             }
             else
             {
-                if(mid+1<n) ans=min(ans,nums[mid+1]);
+                ans=min(ans,arr[mid]);
                 end=mid-1;
-                
             }
         }
         
         return ans;
     }
+    
 };
