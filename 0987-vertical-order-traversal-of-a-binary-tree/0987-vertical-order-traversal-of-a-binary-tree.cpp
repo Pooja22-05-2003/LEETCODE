c00a31f236ab270941104deeede72668470d85c9
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-09 21:25:19 +0530

    Time: 4 ms (56.09%), Space: 15.7 MB (5.23%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,42 +9,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-// TC=O(n)
-// SC=O(n)+O(n)
 class Solution {
 public:
-    void preorder(int y , int level,TreeNode*root, map<int,map<int,multiset<int>>>&mp)
+    void preorder(int col,int row,TreeNode* root,map<int,map<int,multiset<int>>>&mp)
     {
         if(root==NULL) return;
         
-        mp[y][level].insert(root->val);
-        preorder(y-1,level+1,root->left,mp);
-        preorder(y+1,level+1,root->right,mp);
+        mp[col][row].insert(root->val);
+        preorder(col-1,row+1,root->left,mp);
+        preorder(col+1,row+1,root->right,mp);
     }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        // y axis-> {level,{val}}
+        vector<vector<int>>res;
+        
+        // {col->{row/level,{elements at co-ordinate{row,col}}}}
         map<int,map<int,multiset<int>>>mp;
-        vector<vector<int>> ans;
-        if(root==NULL) return ans;
         preorder(0,0,root,mp);
         
-        for(auto y : mp)
+        for(auto col:mp)
         {
             vector<int>temp;
+            for(auto row:col.second)
             
-            for(auto level: y.second)
             {
-                for(auto value:level.second) // level.second means iterating all the values in the particular leve
+                for(auto value:row.second)
+                    
                 {
-                    temp.push_back(value);
+                   temp.push_back(value); 
                 }
             }
-            ans.push_back(temp);
             
+            res.push_back(temp);
         }
         
-        return ans;
-        
+        return res;
     }
 };
