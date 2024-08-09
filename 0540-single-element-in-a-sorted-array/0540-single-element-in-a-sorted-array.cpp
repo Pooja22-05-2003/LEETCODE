ce4d306a0c31cc6c28d23b89af23b75108437e08
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-09 11:38:54 +0530

    Time: 17 ms (72.80%), Space: 24.9 MB (25.38%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& arr) {
-        int n=arr.size();
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+       
         
-        int st=0;
-        int end=n-1;
+        if(nums[0]!=nums[1]) return nums[0];
+        else if(nums[n-1]!=nums[n-2]) return nums[n-1];
         
-        if(n==1) return arr[0];
-        if(st+1<n && arr[0]!=arr[1]) return arr[0];
-        else if(end-1>=0 && arr[n-1]!=arr[n-2]) return arr[n-1];
-        
+         int st=1;
+        int end=nums.size()-2;
         while(st<=end)
         {
             int mid=(st+end)/2;
-            bool isEven = (end-mid)%2==0; // if right part of the arr after mid is even or not.
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
             
-            if(arr[mid] == arr[mid+1]) {
-                if(isEven) {              // if the arr[mid]==arr[mid+1], then now the right part contain the odd number of elements(because we excluded the mid+1, from odd part., neglect the left part and move right to st=mid+2
-                    st = mid+2;
-                } else {
-                    end = mid-1;
-                }
-            } else if(arr[mid] == arr[mid-1]) {
-                if(isEven) { // whole arr length is even, so if the right part contatin the even elements, then including mid, left part contatin the odd, but excluding mid and mid-1, left part contain odd elements, so shrink the search space to left
-                    end = mid-2;
-                } else {
-                    st = mid+1;
-                }
-            } else {
-                return arr[mid];
-            }
+            // even odd [same element]-> ans present in right half, so eliminate left
+            if(((mid%2==0) && (nums[mid]==nums[mid+1])) || ((mid%2==1) && (nums[mid]==nums[mid-1]))) st=mid+1;
+            
+            else end=mid-1;
         }
-    
-    // return arr[end];
+        
         return -1;
-            
     }
 };
