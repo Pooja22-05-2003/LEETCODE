c2f322d23ea85a22b20e927a139bca31bd071210
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-19 20:45:20 +0530

    Time: 17 ms (5.50%), Space: 20.8 MB (5.89%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,47 +1,31 @@
-// DP-Explore all the possible ways and return the max profit
-// Memoization
-// TC=O(n*2)
-// SC=O(n*2) + O(n)
-
-//⭐⭐Here u cannot start the recursion from the back, because buy and sell we need to start from day 0
 class Solution {
 public:
     int solve(int ind, int buy, vector<int>&prices,vector<vector<int>>&dp)
     {
-        // base case
         
         if(ind==prices.size()) return 0;
+        int opt1=0;
+        int opt2=0;
+        int profit=0;
         
         if(dp[ind][buy]!=-1) return dp[ind][buy];
-        int profit=0;
-        if(buy==1)
+        if(buy)
         {
-           // buy the stock on the ith day
-           int opt1=-1*prices[ind]+solve(ind+1,0,prices,dp);
-            
-          // not buy the stock on the ith day
-           int opt2=0+solve(ind+1,1,prices,dp);
-            
-          profit=max(opt1,opt2);
-           
+            opt1=max((-prices[ind]+solve(ind+1,1-buy,prices,dp)),(solve(ind+1,buy,prices,dp)));
+            profit=opt1;
         }
         else
         {
-           // sell the stock on the ith day
-           int opt1=prices[ind]+solve(ind+1,1,prices,dp);
-            
-          // not sell the stock on the ith day
-           int opt2=0+solve(ind+1,0,prices,dp);
-            
-          profit=max(opt1,opt2);
+            opt2=max((+prices[ind]+solve(ind+1,1-buy,prices,dp)),(0+solve(ind+1,buy,prices,dp)));
+            profit=opt2;
         }
         
         return dp[ind][buy]=max(0,profit);
     }
     int maxProfit(vector<int>& prices) {
-       int n=prices.size();
-       vector<vector<int>>dp(n,vector<int>(2,-1));
-       int buy=1; // initilally we can buy any stock
-       return solve(0,buy,prices,dp); 
+        int n=prices.size();
+        bool buy=true;
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return solve(0,buy,prices,dp);
     }
 };
