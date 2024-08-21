49f7994d538662062cf268e9cced568eafc50c7d
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-08-21 09:42:21 +0530

    Time: 86 ms (55.06%), Space: 13.2 MB (51.46%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,15 +3,24 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,1);
-        int maxlen=1;
+        int res=1;
         for(int i=1;i<n;i++)
         {
-            for(int j=i-1;j>=0;j--){
-               if(nums[i]>nums[j]) dp[i]=max(dp[i],1+dp[j]);
+            for(int j=i-1;j>=0;j--)
+            {
+            
+                if(nums[j]<nums[i])
+                {
+                    // cout<<"i:"<<i<<" dp[i]:"<<dp[i]<<" j:"<<j<<" dp[j]:"<<dp[j]<<endl;
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
+                
+                // cout<<dp[i]<<endl;
             }
-            maxlen=max(maxlen,dp[i]);
+            
+            res=max(res,dp[i]);
         }
         
-        return maxlen;
+        return res;
     }
 };
