1b2b0ceb6531d782ab82bb4b6e21934739e27f65
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-09-06 14:28:36 +0530

    Time: 12 ms (37.64%), Space: 21.9 MB (67.09%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,21 +11,20 @@
  */
 class Solution {
 public:
-    int dfsVisit(TreeNode* root)
+    int solve(TreeNode* root)
     {
         if(root==NULL) return 0;
         
-        int lh=dfsVisit(root->left);
-        if(lh==-1) return -1;
+        int lh=solve(root->left);
+        int rh=solve(root->right);
         
-        int rh=dfsVisit(root->right);
-        if(rh==-1) return -1;
+        if((abs(lh-rh))>1) return -1;
         
-        if(abs(lh-rh)>1) return -1;
-        
+        if(lh==-1 || rh==-1) return -1;
         return 1+max(lh,rh);
     }
     bool isBalanced(TreeNode* root) {
-        return dfsVisit(root)!=-1;
+        int ans=solve(root);
+        return (ans!=-1);
     }
 };
