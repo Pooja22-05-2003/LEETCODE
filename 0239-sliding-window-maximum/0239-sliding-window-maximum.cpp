9fdc911d0fd240590c04a55b0e1ea780b1d8482f
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-09-12 22:19:54 +0530

    Time: 164 ms (74.66%), Space: 137.1 MB (92.69%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,30 +1,24 @@
-// TC=O(n)
-// SC=O(n)
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
-        int n=nums.size();
-        // in dequeue we will push the index
-        deque<int>dq; // ⭐dequeue template has name deque and not dequeue
         vector<int>res;
-        
-        for(int i=0;i<n;i++)
+        deque<int>dq;
+        for(int i=0;i<nums.size();i++)
         {
-          
-            
             while(!dq.empty() && dq.front()<=(i-k)) dq.pop_front();
             
-            // dq will maintain the elements in the decreasing order. monotonic decreasing order
-            while(!dq.empty() && nums[dq.back()]<nums[i]) dq.pop_back(); //⭐Here nums[dq.back()] will come and not front
+            while(!dq.empty() && nums[dq.back()]<=nums[i]) dq.pop_back();
+            
+            
             dq.push_back(i);
             
-            if(i>=k-1)
+            
+            if(i-k>=-1)
             {
-              res.push_back(nums[dq.front()]);  
+                res.push_back(nums[dq.front()]);
             }
+            
         }
-        
         return res;
     }
 };
