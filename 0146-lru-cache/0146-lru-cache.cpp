760569febdc44f6fd197f2cb8d1a13a4a05d32a8
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-09-13 11:06:26 +0530

    Time: 380 ms (32.47%), Space: 179 MB (50.23%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -2,10 +2,8 @@ class LRUCache {
 public:
     int size;
     list<int>dll;
-    
-    // {key->{address,value}}
-    unordered_map<int,pair<list<int>::iterator,int>  >mp;
-    
+    unordered_map<int,pair<list<int>::iterator,int>>mp;
+        
     LRUCache(int capacity) {
         size=capacity;
     }
@@ -15,35 +13,45 @@ public:
         dll.erase(mp[key].first);
         dll.push_front(key);
         mp[key].first=dll.begin();
-        
     }
     int get(int key) {
-        if(mp.find(key)!=mp.end()) 
+        if(mp.find(key)!=mp.end())
         {
             makeRecentlyUsed(key);
             return mp[key].second;
         }
-        else return -1;
+        
+        return -1;
     }
     
     void put(int key, int value) {
-        if(mp.find(key)!=mp.end())
-        {
-            makeRecentlyUsed(key);
-            mp[key].second=value;
-        }
+         if(mp.find(key)!=mp.end())
+            {
+                makeRecentlyUsed(key);
+                 mp[key].second=value;
+            }
         else
         {
+            mp[key].second=value;
             dll.push_front(key);
-            mp[key]={dll.begin(),value};
+            mp[key].first=dll.begin();
             size--;
         }
+        
         
         while(size<0)
         {
             mp.erase(dll.back());
             dll.pop_back();
+            
             size++;
         }
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
