23d9a21b623b9ce59b600fbe605663faf55b31f0
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-09-14 19:36:44 +0530

    Time: 11 ms (11.79%), Space: 16.6 MB (39.90%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,53 +4,63 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<vector<int>>dir={{0,-1},{-1,0},{0,1},{1,0}};
-        
-        queue<pair<int,pair<int,int>>>q; // Queue will contain all the rotten oranges
-        
+        int time=0;
+
+        pair<int,int> arr[4]={{0,-1},{-1,0},{0,1},{1,0}};
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,pair<int,int>>>q;
+     
         for(int i=0;i<grid.size();i++)
         {
             for(int j=0;j<grid[0].size();j++)
             {
-                if(grid[i][j]==2) q.push({0,{i,j}});
+                if(grid[i][j]==2) 
+                {
+                    q.push({0,{i,j}});
+                    vis[i][j]=1;
+                }
             }
         }
+        
+        // cout<<"size:"<<q.size()<<endl;
         
         int mintime=0;
         while(!q.empty())
         {
-            auto curr=q.front();
-            q.pop();
+            auto curr=q.front();q.pop();
+            int r=curr.second.first;
+            int c=curr.second.second;
             int time=curr.first;
-            int row=curr.second.first;
-            int col=curr.second.second;
-            
+            mintime=time;
+            // cout<<"mintime:"<<mintime<<endl;
             for(int i=0;i<4;i++)
             {
-                int newrow=row+dir[i][0];
-                int newcol=col+dir[i][1];
-                
-                if((newrow>=0 && newrow<n && newcol>=0 && newcol<m) && (grid[newrow][newcol]==1 && vis[newrow][newcol]==0))
+                int nr=r+arr[i].first;
+                int nc=c+arr[i].second;
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && vis[nr][nc]==0)
                 {
-                    vis[newrow][newcol]=1;
-                    q.push({time+1,{newrow,newcol}});
-                    mintime=time+1;
+                    vis[nr][nc]=1;
+                    grid[nr][nc]=2;
+                    // cout<<"nr:"<<nr<<" nc:"<<nc<<endl;
+                    q.push({time+1,{nr,nc}});
                 }
+  
             }
             
         }
-        
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
+         for(int i=0;i<grid.size();i++)
             {
-                if(grid[i][j]==1 && vis[i][j]==0) return -1;
+                for(int j=0;j<grid[0].size();j++)
+                {
+                    if(grid[i][j]==1) 
+                    {
+                        // cout<<"i:"<<i<<" j:"<<j<<endl;
+                        return -1;
+                    }
+                }
             }
-        }
         
         return mintime;
-        
+     
     }
 };
