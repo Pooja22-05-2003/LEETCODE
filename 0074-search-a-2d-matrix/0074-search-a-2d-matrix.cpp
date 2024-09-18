f296e1e8e6df540fdce0fdc5632321ca8a705bd1
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-09-18 17:55:31 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (42.58%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& arr, int target) {
-        int n=arr.size();
-        int m=arr[0].size();
+        int row=arr.size();
+        int col=arr[0].size();
+        
         int st=0;
-        int end=n-1;
+        int end=row-1;
         
         while(st<=end)
         {
             int mid=(st+end)/2;
             
-            // check whether the element is present in the current row or not
-            if(arr[mid][0]<=target && arr[mid][m-1]>=target)
+            // check whether the ans lies in the current row or not
+            if(arr[mid][0]<=target && arr[mid][col-1]>=target)
             {
-                
-//                 for(int i=0;i<m;i++) 
-//                 {
-//                     if(arr[mid][i]==target) return true;
-//                 }
-//                 return false;
-                
                 int low=0;
-                int high=m-1;
+                int high=col-1;
                 
                 while(low<=high)
                 {
-                    int r=(low+high)/2;
-                    if(arr[mid][r]==target) return true;
-                    if(arr[mid][r]<target) low=r+1;
-                    else high=r-1;
+                    int m=(low+high)/2;
+                    if(arr[mid][m]==target) return true;
+                    else if(arr[mid][m]>target) high=m-1;
+                    else low=m+1;
                 }
+                
                 return false;
-                
             }
-            else if(arr[mid][0]>target) end=mid-1;
+            
+            else if(target<arr[mid][0]) end=mid-1;
             else st=mid+1;
-                
         }
+        
         return false;
+        
     }
 };
