b2640ac0f4be5beedeb8f7390d0fef4fd824926e
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-09-20 17:54:17 +0530

    Time: 39 ms (19.21%), Space: 64.8 MB (6.81%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    bool check(int k,vector<int>& arr, int h)
+    bool check(int k, vector<int>arr, int h)
     {
-        int t=0;
-        for(auto it: arr)
-        {
-            t+=(it/k)+((it%k)!=0);
-            if(t>h) break;
-        } 
-      
-        return (t<=h);
-            
-    }
-    int minEatingSpeed(vector<int>& arr, int h) {
+        int time=0;
         
-        int n=arr.size();
-        int maxx=INT_MIN;
-        for(auto it: arr)  maxx=max(maxx,it);
+        for(int i=0;i<arr.size();i++)
+        {
+            time+=(arr[i]%k==0)? arr[i]/k: (arr[i]/k)+1;
+            if(time>h) break;
+        }
+        
+        return (time<=h);
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int min_el=INT_MAX; int max_el=INT_MIN;
+        
+        for(auto it:piles)
+        {
+            min_el=min(min_el,it);
+            max_el=max(max_el,it);
+        }
+        
         int st=1;
-        int end=maxx;
+        int end=max_el;
         
         while(st<=end)
         {
             int mid=(st+end)/2;
-            if(mid==1 && check(mid,arr,h)==true) return mid;
-            if(check(mid,arr,h)==true && check(mid-1,arr,h)==false) return mid;
-            if(check(mid,arr,h)==true) end=mid-1;
+            
+            if(mid==1 &&  check(mid,piles,h)==true) return mid;
+            if( check(mid,piles,h)==true && check(mid-1,piles,h)==false) return mid;
+            else if(check(mid,piles,h)==true) end=mid-1;
             else st=mid+1;
-         
         }
         
         return -1;
+            
     }
 };
