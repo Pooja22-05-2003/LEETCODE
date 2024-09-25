4779a4d967e96c0a26d784cb8a6267904ad4eed2
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-09-25 18:46:39 +0530

    Time: 1730 ms (11.87%), Space: 421.2 MB (12.48%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,32 +1,16 @@
-/*
-Amazon-4,Mathworks-3,Google-2
-*/
 class Solution {
 public:
-   
+    int solve(int i, int j, string s, string t,vector<vector<int>>&dp)
+    {
+        if(j==t.size()) return 1;
+        if(i==s.size()) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==t[j]) return dp[i][j]=solve(i+1,j+1,s,t,dp)+solve(i+1,j,s,t,dp);
+        else return dp[i][j]=solve(i+1,j,s,t,dp);
+    }
     int numDistinct(string s, string t) {
-        int n1=s.size();
-        int n2=t.size();
-        vector<double>curr(n2+1,0);
-        vector<double>prev(n2+1,0);
-        
-        
-        // right shifting , because base case consider <0
-        
-        // for any index i, if the j is at 0 then we return 1
-        prev[0]=1;
-        curr[0]=1;//⭐ I missed this case
-        
-        for(int i=1;i<=n1;i++)
-        {
-            for(int j=1;j<=n2;j++)
-            {
-                   
-                    if(s[i-1]==t[j-1]) curr[j]=prev[j-1]+prev[j];
-                    else curr[j]=prev[j];
-            }
-            prev=curr;
-        }
-        return (int)prev[n2];
+        vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
+        return solve(0,0,s,t,dp);
     }
 };
