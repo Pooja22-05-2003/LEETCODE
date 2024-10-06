3717bf8e147cfb5dd1f959652ebb667a3ce793bf
Pooja Kumari <106443950+Pooja22-05-2003@users.noreply.github.com>
2024-10-06 20:38:14 +0530

    Time: 7 ms (31.64%), Space: 16.3 MB (10.71%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,79 +1,61 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
 class Solution {
 public:
-    unordered_map<TreeNode*, TreeNode*>mp;
-    
-    void BFS(TreeNode* target, int k, vector<int>&res)
-    {
-        set<TreeNode*>vis;
-        
-        queue<TreeNode*>q;
-        q.push(target);
-        vis.insert(target);
-        
-        while(!q.empty())
-        {
-            if(k==0) break;
-            
-            int n=q.size();
-            while(n--)
-            {
-                auto curr=q.front();
-                q.pop();
-                
-                if(curr->left && vis.find(curr->left)==vis.end())
-                {
-                    q.push(curr->left);
-                    vis.insert(curr->left);
-                }
-                
-                 if(curr->right && vis.find(curr->right)==vis.end())
-                {
-                    q.push(curr->right);
-                    vis.insert(curr->right);
-                }
-                
-                if(mp.count(curr) && vis.find(mp[curr])==vis.end())
-                {
-                    q.push(mp[curr]);
-                    vis.insert(mp[curr]);
-                }
-            }
-            k--;
-                
-                
-        }
-        
-        
-        while(!q.empty())
-        {
-            res.push_back(q.front()->val);
-            q.pop();
-        }
-        
-    }
-    void inorder(TreeNode* root)
+    void preorder(unordered_map<TreeNode*, TreeNode*>&mp, TreeNode* root,TreeNode*parent)
     {
         if(root==NULL) return;
+        mp[root]=parent;
         
-        if(root->left) mp[root->left]=root;
-        inorder(root->left);
-        if(root->right) mp[root->right]=root;
-        inorder(root->right);
+        preorder(mp,root->left,root);
+        preorder(mp,root->right,root);
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        inorder(root);
+        unordered_map<TreeNode*, TreeNode*>mp;
+        unordered_set<TreeNode*>vis;
+        preorder(mp,root,NULL);
+        
+        queue<pair<TreeNode*,int>>q;
+        vis.insert(target);
+        q.push({target,0});
         
         vector<int>res;
-        BFS(target,k,res);
+        
+        while(!q.empty()){
+            int size=q.size();
+            int level=q.front().second;
+
+            if(level==k) 
+            {
+                vector<int>res;
+                while(size--)
+                {
+                   auto curr=q.front().first;q.pop();
+                   res.push_back(curr->val);
+                    
+                }
+                return res;
+            }
+            
+            while(size--){
+            TreeNode* curr=q.front().first;
+            int level=q.front().second;
+            q.pop();
+            if(curr->left && vis.find(curr->left)==vis.end()){
+                q.push({curr->left,level+1});
+                vis.insert(curr->left);
+            }
+            if(curr->right && vis.find(curr->right)==vis.end()){
+                q.push({curr->right,level+1});
+                vis.insert(curr->right);
+            }
+            
+            if(mp[curr]!=NULL  && vis.find(mp[curr])==vis.end()){
+                q.push({mp[curr],level+1});
+                vis.insert(mp[curr]);
+             }
+            }
+        }
+        
         return res;
     }
 };
